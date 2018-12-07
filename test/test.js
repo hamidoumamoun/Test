86fc4fe2f22e30849814557ec8099501158d7450
hamidou barry <barryhamidou8@gmail.com>
2018-12-07 11:46:31 +0100

    factoring

--- test/test.js
+++ test/test.js
@@ -1,10 +1,7 @@
-var assert = require('assert');
+var assert = require("assert");
 
-
-
-
-describe('addition',function(){
-it ('verifier que 2+2 = 4', function(){
+describe("addition",function(){
+it ("verifier que 2+2 = 4", function(){
     assert.equal(4,2+2);
 });
 })
